03a28a284051115880aa1c6409eb50111470b45f
Mike Hickman <mike@pdxmike.com>
2020-09-04 14:42:48 -0700

    Add initial mobile view for organization index
    - Update seeds file to provide longer org description
    - Create basic card styling
    - Show number of search results

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -5,12 +5,10 @@ class OrganizationsController < ApplicationController
     @search_name = params[:name]
     @organizations = Organization.order(:name)
     @organizations = @organizations.search_by_name(@search_name) if @search_name.present?
+    @search_result_count = @organizations.count
   end
 
   def show
     @organization = Organization.find(params[:id])
   end
-
-  private
-
 end
